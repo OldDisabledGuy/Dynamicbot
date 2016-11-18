4bf7e3cc149bcaa3f154b6a2f4fae34b4dd90937
kaboberaser <dynamichappenings@gmail.com>
2016-11-18 18:22:28 +0100

    added {szcz, import random and updated {help

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+import random
 
 client = discord.Client()
 
@@ -11,6 +12,7 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+
 @client.event
 async def on_message(message):
     if message.content.startswith('{test'):
@@ -20,12 +22,15 @@ async def on_message(message):
             if log.author == message.author:
                 counter += 1
         await client.edit_message(tmp, 'you have {} messages'.format(counter))
+    elif message.content.startswith ('{help'):
+        await client.send_message(message.channel, 'commands: `{test`, `{sleep`, `{lmdo`, `{szcz`')
     elif message.content.startswith('{sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'done sleeping')
     elif message.content.startswith('{lmdo'):
         await client.send_message(message.channel, 'Lmdo')
-    elif message.content.startswith ('{help'):
-        await client.send_message(message.channel, 'commands: `{test`, `{sleep`, `{lmdo`')
+    elif message.content.startswith ('{szcz'):
+        szcz = ['szczur', 'szczupak', 'chrząszcz', 'szczecin', 'szczebrzeszyn', 'szczerość', 'szczekać', 'szczęka', "pszczoła", 'oszczędzać', 'płaszcz', 'szczegóły', 'puszcza', 'paszcza', 'chińszczyzna', 'polszczyzna', 'barszcz', 'szczeniak', 'szczep', 'szczyt']
+        await client.send_message(message.channel, random.choice(szcz))
 
 client.run('MjQ4ODc4MTY5NzA3MjQ5Njc0.Cw-J2w.e8ebHynG1fd3WnCV8JSn5O6CPBs')
