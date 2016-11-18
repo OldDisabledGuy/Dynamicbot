eb19fb018cb4e25761fe2ed76e1d3da042f7fb63
kaboberaser <dynamichappenings@gmail.com>
2016-11-18 16:15:57 +0100

    changed prefix and added {help

--- bot.py
+++ bot.py
@@ -12,18 +12,19 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
+    if message.content.startswith('{test'):
         counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        tmp = await client.send_message(message.channel, 'calculating messages')
         async for log in client.logs_from(message.channel, limit=100):
             if log.author == message.author:
                 counter += 1
-
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
+        await client.edit_message(tmp, 'you have {} messages'.format(counter))
+    elif message.content.startswith('{sleep'):
         await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-    elif message.content.startswith('!lmdo'):
+        await client.send_message(message.channel, 'done sleeping')
+    elif message.content.startswith('{lmdo'):
         await client.send_message(message.channel, 'Lmdo')
+    elif message.content.startswith ('{help'):
+        await client.send_message(message.channel, 'commands: `{test`, `{sleep`, `{lmdo`')
 
 client.run('MjQ4ODc4MTY5NzA3MjQ5Njc0.Cw-J2w.e8ebHynG1fd3WnCV8JSn5O6CPBs')
