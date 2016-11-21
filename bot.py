a465c35bd6e1d1b41fc8dac36b8c6d7b9984614f
br0b <bulbadontreal@gmail.com>
2016-11-21 16:27:23 +0200

    fixed {say

--- bot.py
+++ bot.py
@@ -17,39 +17,37 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('{test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'calculating messages')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-        await client.edit_message(tmp, 'you have {} messages'.format(counter))
-    elif message.content.startswith ('{help'):
-        await client.send_message(message.channel, 'commands: `{test`, `{snooze`, `{lmdo`, `{szcz`, `{smug`, `{sacroni`, `{say`')
-    elif message.content.startswith('{snooze'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'done snoozing')
-    elif message.content.startswith('{lmdo'):
-        await client.send_message(message.channel, 'Lmdo')
-    elif message.content.startswith ('{szcz'):
-        szczs = open(dr + "\\szcz.txt", "r")
-        szczlist = szczs.readlines()
-        await client.send_message(message.channel, random.choice(szczlist))
-    elif message.content.startswith ('{smug'):
-        smug = os.listdir(dr + "\\smug")
-        smug.remove('Thumbs.db')
-        await client.send_file(message.channel, dr + '\\smug\\' + random.choice(smug))
-    elif message.content.startswith ('{sacroni'):
-        sacroni = os.listdir(dr + "\\sacroni")
-        sacroni.remove('Thumbs.db')
-        await client.send_file(message.channel, dr + '\\sacroni\\' + random.choice(sacroni))
-    elif message.content.startswith ('{say'):
-        say = message.content[len('{say'):].strip()
-        if say != '':
-            if message.author.bot == True:
-                await client.send_message(message.channel, "pwned kiddo")
-            else:
-                await client.send_message(message.channel, say)
+	if message.author.bot == False:
+	    if message.content.startswith('{test'):
+	        counter = 0
+	        tmp = await client.send_message(message.channel, 'calculating messages')
+	        async for log in client.logs_from(message.channel, limit=100):
+	            if log.author == message.author:
+	                counter += 1
+	        await client.edit_message(tmp, 'you have {} messages'.format(counter))
+	    elif message.content.startswith ('{help'):
+	        await client.send_message(message.channel, 'commands: `{test`, `{snooze`, `{lmdo`, `{szcz`, `{smug`, `{sacroni`, `{say`')
+	    elif message.content.startswith('{snooze'):
+	        await asyncio.sleep(5)
+	        await client.send_message(message.channel, 'done snoozing')
+	    elif message.content.startswith('{lmdo'):
+	        await client.send_message(message.channel, 'Lmdo')
+	    elif message.content.startswith ('{szcz'):
+	        szczs = open(dr + "\\szcz.txt", "r")
+	        szczlist = szczs.readlines()
+	        await client.send_message(message.channel, random.choice(szczlist))
+	    elif message.content.startswith ('{smug'):
+	        smug = os.listdir(dr + "\\smug")
+	        smug.remove('Thumbs.db')
+	        await client.send_file(message.channel, dr + '\\smug\\' + random.choice(smug))
+	    elif message.content.startswith ('{sacroni'):
+	        sacroni = os.listdir(dr + "\\sacroni")
+	        sacroni.remove('Thumbs.db')
+	        await client.send_file(message.channel, dr + '\\sacroni\\' + random.choice(sacroni))
+	    elif message.content.startswith ('{say'):
+	        say = message.content[len('{say'):].strip()
+	        if say != '':
+	     	   await client.send_message(message.channel, say)
 
 
 client.run('MjQ4ODc4MTY5NzA3MjQ5Njc0.Cw-J2w.e8ebHynG1fd3WnCV8JSn5O6CPBs')
