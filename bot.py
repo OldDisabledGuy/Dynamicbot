aa29d6f7c9b6fce379f60f8b8537ec59d3b49e43
kaboberaser <dynamichappenings@gmail.com>
2016-12-11 12:51:58 +0100

    added onmemberjoin and onmemberremove

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 import cleverbot
 
 client = discord.Client()
-dr = os.getcwd()
+dr=os.getcwd()
 cb = cleverbot.Cleverbot()
 
 @client.event
@@ -17,6 +17,13 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+@client.event
+async def on_member_join(member):
+	await client.send_message(discord.Object(id=member.server.id), 'user **' + str(member)[:-5] + '** has joined the server')
+	
+@client.event
+async def on_member_remove(member):
+	await client.send_message(discord.Object(id=member.server.id), 'user **' + str(member)[:-5] + '** has left the server')
 
 @client.event
 async def on_message(message):
@@ -28,36 +35,35 @@ async def on_message(message):
 				if log.author == message.author:
 					counter += 1
 			await client.edit_message(tmp, 'you have {} messages'.format(counter))
-		elif message.content.startswith('{help'):
-			await client.send_message(message.channel, 'commands: `{test`, `{snooze`, `{lmdo`, `{szcz`, `{smug`, `{sacroni`, `{proofs`, `{say`')
+		elif message.content.startswith ('{help'):
+			await client.send_message(message.channel, 'commands: `{test`, `{snooze`, `{lmdo`, `{szcz`, `{smug`, `{sacroni`, `{proofs`, `{say`, `{cb`')
 		elif message.content.startswith('{snooze'):
 			await asyncio.sleep(5)
 			await client.send_message(message.channel, 'done snoozing')
 		elif message.content.startswith('{lmdo'):
 			await client.send_message(message.channel, 'Lmdo')
-		elif message.content.startswith('{szcz'):
+		elif message.content.startswith ('{szcz'):
 			szczs = open(dr + "\\szcz.txt", "r")
 			szczlist = szczs.readlines()
 			await client.send_message(message.channel, random.choice(szczlist))
-		elif message.content.startswith('{smug'):
+		elif message.content.startswith ('{smug'):
 			smug = os.listdir(dr + "\\smug")
 			smug.remove('Thumbs.db')
 			await client.send_file(message.channel, dr + '\\smug\\' + random.choice(smug))
-		elif message.content.startswith('{sacroni'):
+		elif message.content.startswith ('{sacroni'):
 			sacroni = os.listdir(dr + "\\sacroni")
 			sacroni.remove('Thumbs.db')
 			await client.send_file(message.channel, dr + '\\sacroni\\' + random.choice(sacroni))
-		elif message.content.startswith('{proofs'):
+		elif message.content.startswith ('{proofs'):
 			proofster = os.listdir(dr + "\\proofster")
 			proofster.remove('Thumbs.db')
 			await client.send_file(message.channel, dr + '\\proofster\\' + random.choice(proofster))
-		elif message.content.startswith('{say'):
+		elif message.content.startswith ('{say'):
 			say = message.content[len('{say'):].strip()
 			if say != '':
 				await client.send_message(message.channel, say)
 		elif message.content.startswith('{cb'):
 			cl = message.content[len('{cb'):].strip()
 			await client.send_message(message.channel, cb.ask(cl))
-
 
 client.run('MjQ4ODc4MTY5NzA3MjQ5Njc0.Cw-J2w.e8ebHynG1fd3WnCV8JSn5O6CPBs')
